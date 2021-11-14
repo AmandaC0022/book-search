7af43193cb7c6576966e47e2501ff91e891a886e
Amanda Morgan <AmandaC0022@gmail.com>
2021-11-14 10:28:11 -0600

    updated server.js to include Apollo Server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+
+//Don't need express routes, we will be replacing with React Router
+// const routes = require('./routes');
+
+const { ApolloServer } = require('apollo-server-express'); 
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+}); 
+
 const PORT = process.env.PORT || 3001;
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +27,15 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
