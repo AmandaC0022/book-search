e943f947507af01ac8aca2f67be65a1ecd28f607
Amanda Morgan <AmandaC0022@gmail.com>
2021-11-17 16:39:38 -0600

    trying to deploy to heroku

--- server/server.js
+++ server/server.js
@@ -2,44 +2,45 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 
-import { ApolloServer } from 'apollo-server-express';
-import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
-import express from 'express';
-import http from 'http';
+// import { ApolloServer } from 'apollo-server-express';
+// import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+// import express from 'express';
+// import http from 'http';
 
-// const { ApolloServer } = require('apollo-server-express'); 
-// const { typeDefs, resolvers } = require('./schemas');
-// const { authMiddleware } = require('./utils/auth');
+const { ApolloServer } = require('apollo-server-express'); 
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const server = new ApolloServer({
-//     typeDefs,
-//     resolvers,
-//     context: authMiddleware,
-// }); 
-
-//updated to the new apollo server 
-async function startApolloServer(typeDefs, resolvers) {
-  const app = express();
-  const httpServer = http.createServer(app);
-  const server = new ApolloServer({
+const server = new ApolloServer({
     typeDefs,
     resolvers,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-  });
+    context: authMiddleware,
+}); 
 
-  await server.start();
-  server.applyMiddleware({ app });
-  await new Promise(resolve => httpServer.listen(PORT, resolve));
-  console.log(`API server running on port ${PORT}!`);
-  console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
-}
+//updated to the new apollo server 
+// async function startApolloServer(typeDefs, resolvers) {
+//   const app = express();
+//   const httpServer = http.createServer(app);
+//   const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+//   });
+
+//   await server.start();
+//   server.applyMiddleware({ app });
+//   await new Promise(resolve => httpServer.listen(PORT, resolve));
+//   console.log(`API server running on port ${PORT}!`);
+//   console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+// }
+
 //require middleware
-// server.applyMiddleware({ app });
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+server.applyMiddleware({ app });
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -50,9 +51,9 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-// db.once('open', () => {
-//   app.listen(PORT, () => {
-//     console.log(`API server running on port ${PORT}!`);
-//     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-//   });
-// });
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
