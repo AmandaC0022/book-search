dcd4709f3f19758f630a39d4d32fd555ac918b18
Amanda Morgan <AmandaC0022@gmail.com>
2021-11-17 14:45:06 -0600

    added SAVE_BOOK mutation to search books component

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,23 +2,19 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { useMutation } from '@apollo/client'; 
+import { SAVE_BOOK } from '../utils/mutations'; 
 
 const SearchBooks = () => {
-  // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
-  // create state for holding our search field data
   const [searchInput, setSearchInput] = useState('');
-
-  // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
-
-  // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
-  // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
+  const [saveBook, { error }] = useMutation(SAVE_BOOK); 
+  
   useEffect(() => {
-    return () => saveBookIds(savedBookIds);
-  });
+    return () => saveBookIds(savedBookIds); 
+  })
 
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
@@ -29,8 +25,11 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await searchGoogleBooks(searchInput);
-
+      //make a call to Google's API 
+      const response = await fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=${searchInput}`
+      ); 
+      //if response isn't ok, throw an error
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
@@ -45,7 +44,9 @@ const SearchBooks = () => {
         image: book.volumeInfo.imageLinks?.thumbnail || '',
       }));
 
+      //update setSearchedBooks with new data returned from the API call 
       setSearchedBooks(bookData);
+      //reset the input value back to be empty 
       setSearchInput('');
     } catch (err) {
       console.error(err);
@@ -65,11 +66,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      const { data } = await saveBook({
+        variables: { bookData: { ...bookToSave } }, 
+      });
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
