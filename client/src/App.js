28a05d2ea193a59e5e594430014699aa1bc8e58c
Amanda Morgan <AmandaC0022@gmail.com>
2021-11-17 11:55:01 -0600

    added react router to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,57 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from 'apollo/client/link/context'; 
+
+//Components 
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+//create our API endpoint 
+const httpLink = createHttpLink({
+  uri: '/graphql', 
+}); 
+
+const authLink = setContext((_, { headers }) => {
+  //grab token from localstorage 
+  const token = localStorage.getItem('id_token'); 
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Owner ${token}` : '', 
+    }
+  };
+});
+
+//create our new Apollo Client which uses the auth middleware before calling to our API endpoint 
+const client = new ApolloClient({
+  link: authLink.concat(httpLink), 
+  cache: new InMemoryCache(), 
+}); 
+
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
+    <ApolloProvider client={client}>
+      <Router>
+      <Navbar />
         <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
+          <Route exact path='/'>
+            <SearchBooks /> 
+          </Route>
+          <Route exact path='/saved'>
+            <SavedBooks /> 
+          </Route>
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Switch>
-      </>
     </Router>
+    </ApolloProvider>
   );
 }
 
